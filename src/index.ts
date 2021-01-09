a0ddcade709792f8d4b195e9899be960c7753e4c
Damião Castro <damiao.c@hotmail.com>
2021-01-09 20:57:03 +0100

    try this way

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
 const core = require('@actions/core');
+
 const nameToGreet = core.getInput('who-to-greet');
 console.log(`Hello ${nameToGreet.length}!`);
+
 // for (var element in process.env) {
 //     core.log(`${element}: ${process.env[element].length}`);
 // };
+
