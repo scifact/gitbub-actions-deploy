f180f0e815903004adf54d0daf4a06a2504f8ecc
Damião Castro <damiao.c@hotmail.com>
2021-01-09 21:16:40 +0100

    another module

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
-define("index", ["require", "exports", "@actions/core"], function (require, exports, core) {
+System.register("index", ["@actions/core"], function (exports_1, context_1) {
     "use strict";
-    Object.defineProperty(exports, "__esModule", { value: true });
-    const nameToGreet = core.getInput('who-to-greet');
-    console.log(`Hello ${nameToGreet.length}!`);
+    var core, nameToGreet;
+    var __moduleName = context_1 && context_1.id;
+    return {
+        setters: [
+            function (core_1) {
+                core = core_1;
+            }
+        ],
+        execute: function () {
+            nameToGreet = core.getInput('who-to-greet');
+            console.log(`Hello ${nameToGreet.length}!`);
+            // for (var element in process.env) {
+            //     core.log(`${element}: ${process.env[element].length}`);
+            // };
+        }
+    };
 });
-// for (var element in process.env) {
-//     core.log(`${element}: ${process.env[element].length}`);
-// };
