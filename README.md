77025f1b3ed5a3777e00ab0a13a3e3ae171a13db
Damião Castro <damiao.c@hotmail.com>
2021-01-11 21:47:46 +0100

    stuff

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # gitbub-actions-deploy
+
+https://github.com/actions/hello-world-javascript-action
+
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,24 @@
 import * as core from '@actions/core';
+import * as  github from '@actions/github';
+var fs = require('fs');
 
-const nameToGreet = core.getInput('who-to-greet');
-console.log(`Hello ${nameToGreet.length}!`);
+console.log(JSON.stringify(github.context));
+
+fs.readdir('./', function (err, files) {
+    //handling error
+    if (err) {
+        return console.log('Unable to scan directory: ' + err);
+    } 
+    //listing all files using forEach
+    files.forEach(function (file) {
+        // Do whatever you want to do with the file
+        console.log(file); 
+    });
+});
+
+// core.getInput
+// const nameToGreet = core.getInput('who-to-greet');
+// console.log(`Hello ${nameToGreet.length}!`);
 
 // for (var element in process.env) {
 //     core.log(`${element}: ${process.env[element].length}`);
